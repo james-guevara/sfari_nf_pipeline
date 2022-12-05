3b4e16722ac6983b1e5c8b38f8e3739bc724b978
jag2231 <guevara.james@gmail.com>
2022-12-05 11:20:54 -0800

    added another config (for mac/docker)

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,3 @@
-# nextflow run workflows/run_vep.nf --fasta /expanse/projects/sebat1/vep_resources/SFARI/iWGS_v1/resources/GRCh38_full_analysis_set_plus_decoy_hla.fa --ped /expanse/lustre/projects/ddp195/eiovino/ita_cohort/SNVs/ita_fam_nov_sorted.ped  --vcfs test_folder --vep_config vep.ini -entry RUN_VEP -profile slurm
-# nextflow run workflows/run_vep.nf --fasta /expanse/projects/sebat1/vep_resources/SFARI/iWGS_v1/resources/GRCh38_full_analysis_set_plus_decoy_hla.fa --ped /expanse/lustre/projects/ddp195/eiovino/ita_cohort/SNVs/ita_fam_nov_sorted.ped  --vcfs test_folder --vep_config vep.ini -entry RUN_VEP -profile slurm -stub
-
-nextflow run workflows/run_vep.nf --fasta /expanse/projects/sebat1/vep_resources/SFARI/iWGS_v1/resources/GRCh38_full_analysis_set_plus_decoy_hla.fa --ped /expanse/lustre/projects/ddp195/eiovino/ita_cohort/SNVs/ita_fam_nov_sorted.ped  --vcfs test_folder --vep_config vep.ini -entry RUN_VEP -profile slurm
-
 nextflow run main.nf --fasta /expanse/projects/sebat1/vep_resources/SFARI/iWGS_v1/resources/GRCh38_full_analysis_set_plus_decoy_hla.fa --ped /expanse/lustre/projects/ddp195/eiovino/ita_cohort/SNVs/ita_fam_nov_sorted.ped  --vcfs test_folder --vep_config vep.ini -entry RUN_VEP -profile slurm
+
+nextflow run main.nf --fasta /expanse/projects/sebat1/vep_resources/SFARI/iWGS_v1/resources/GRCh38_full_analysis_set_plus_decoy_hla.fa --ped /expanse/lustre/projects/ddp195/eiovino/ita_cohort/SNVs/ita_fam_nov_sorted.ped  --vcfs test_folder --vep_config vep.ini -entry  -profile slurm
